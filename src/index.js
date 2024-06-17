226439553318b122b64c9163b47189042b112975
Miracle <miracleigbunuogheneekpu@gmail.com>
2024-06-18 00:10:56 +0100

    completed

--- src/index.js
+++
@@ -1,6 +0,0 @@
-var hambuger = document.getElementById("hambuger");
-var links = document.getElementById("links");
-
-hambuger.addEventListener("click", () => {
-  links.classList.toggle("active");
-});
